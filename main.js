9b808a50216ae263f78e6120ef03e30d6028729c
sonicakes <soniaparker1207@gmail.com>
2018-06-26 14:43:02 +1000

    commit more logic

--- main.js
+++ main.js
@@ -9,14 +9,28 @@ $(document).ready(function() {
     $(".box").html("");
   }
   newGame();
+  //winning combos
+  //if boxes id coincide with the winning combos, we have to declare winner and end game
+  const win1 = [0, 1, 2];
+  const win2 = [2, 4, 6];
+  const win3 = [0, 4, 8];
+  const win4 = [3, 4, 5];
+  const win5 = [6, 7, 8];
   //2. add jquery click event to boxes that:
   //2a.sets inner html to X or 0, depending on our currentPlayer
   //2b.swaps currentPlayer
   let emptySquares = 9;
   //construct an array via new Array(9) method
   const arrayOfValues = [];
+  const arrayOfIds = [];
   // A $( document ).ready() block.
+  //compare with winning combos function
+  const compare = function() {
+    const toCompare = arrayOfIds.join("");
+    console.log(toCompare);
+  }
 
+  //turn
   //we call the function newGame() here, otherwise it wouldnt be available!
 
   //function that handles what happens after you click on any square
@@ -31,15 +45,28 @@ $(document).ready(function() {
     }
 
     arrayOfValues.push($(this).text());
+    arrayOfIds.push($(this).attr("id"));
     console.log(arrayOfValues);
+    console.log(arrayOfIds);
+    //reduces number of empty squares
     emptySquares--;
     console.log(emptySquares);
   };
+  //function to hold the logic of the game
 
+  $(".box").on("click", whenClicked);
+  // if ()
+
+  //while (there are empty squares)
+  //place a marker
+  //reduce number of squares
+  //if(there is a three combo)
+  //exit the game, declare WINNER
+  //exit game with a draw
   //we can only click if there are empty squares, i.e array.length=<9
 
 
-  $(".box").on("click", whenClicked);
+
 
 
 
