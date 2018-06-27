cd4408fab7c9413464d9902e61a790c0fc3b2e76
sonicakes <soniaparker1207@gmail.com>
2018-06-27 19:15:21 +1000

    commit working version of the game

--- main.js
+++ main.js
@@ -2,99 +2,139 @@ $(document).ready(function() {
   console.log("ready!");
 
   let currentPlayer = "X";
+  let isGameActive = false;
+  // let emptySquares = 9;
 
-  const newGame = function() {
-
-    currentPlayer = "X";
-
-    $(".box").html("");
-  }
-
-  newGame();
-  const whenClicked = function() {
-
-    // TODO Check if square that was clicked is empty
-    // TODO If it is not empty, we need to stop and not do anything else
-
-    if (currentPlayer === "X") {
-      $(this).text("X");
-      currentPlayer = "0";
-      arrayOfCrosses.push($(this).text());
-      idsOfCrosses.push($(this).attr("id"));
-    } else {
-      $(this).text("0");
-      currentPlayer = "X";
-      arrayOfNods.push($(this).text());
-      idsOfNods.push($(this).attr("id"));
-    }
-
-
-    arrayOfValues.push($(this).text());
-    arrayOfIds.push($(this).attr("id"));
-
-
-    if (idsOfNods.includes("0") && idsOfNods.includes("1") && idsOfNods.includes("2")) {
-      console.log(`0 wins`);
-      //TODO whyit doesnt work???????
-      for (var i = 0; i < idsOfNods.length; i++) {
-        debugger;
-        $(idsOfNods[i]).addClass("greeny");
-      }
-      return `0 wins`;
-    }
-
-    if (idsOfNods.includes("2") && idsOfNods.includes("4") && idsOfNods.includes("6")) {
-      console.log(`0 wins`);
-      return `0 wins`;
-    }
-    if (idsOfNods.includes("0") && idsOfNods.includes("4") && idsOfNods.includes("8")) {
-      console.log(`0 wins`);
-      return `0 wins`;
-    }
-    if (idsOfNods.includes("3") && idsOfNods.includes("4") && idsOfNods.includes("5")) {
-      console.log(`0 wins`);
-      return `0 wins`;
-    }
-    if (idsOfNods.includes("6") && idsOfNods.includes("7") && idsOfNods.includes("8")) {
-      console.log(`0 wins`);
-      return `0 wins`;
-    }
-    console.log(`this is the values we got from clicking:${arrayOfValues}`);
-    console.log(`this is the ids array: ${arrayOfIds}`);
-    console.log(`this is how many nods we have so far: ${arrayOfNods} and their ids are the following:`);
-    console.log(`${idsOfNods}`);
-    console.log(`this is how many crosses we got so far:${arrayOfCrosses} and their ids are the following:`);
-    console.log(`${idsOfCrosses}`);
-    //reduces number of empty squares
-    emptySquares--;
-    console.log(`this is how many empty squares we have got left: ${emptySquares}`);
-    console.log("**********************click again*****************");
-
-  }
   const win1 = [0, 1, 2];
-  const win2 = [2, 4, 6];
+  const win2 = [2, 4, 6]; //diagonal
   const win3 = [0, 4, 8];
   const win4 = [3, 4, 5];
   const win5 = [6, 7, 8];
 
-  const arrayOfValues = [];
-  const arrayOfNods = [];
-  const arrayOfCrosses = [];
-  const arrayOfIds = [];
-  const idsOfNods = [];
-  const idsOfCrosses = [];
+  // const arrayOfValues = [];
+  // const arrayOfNods = [];
+  // const arrayOfCrosses = [];
+  // const arrayOfIds = [];
+  let idsOfNods = [];
+  let idsOfCrosses = [];
+
+  const newGame = function() {
+    // X is always the first player in any game
+    currentPlayer = "X";
+
+    // Clear all the squares
+    $(".box").html("");
+
+    // Set the game state to active
+    isGameActive = true;
+
+    // Clear the arrays of X and 0
+    idsOfNods = [];
+    idsOfCrosses = [];
+
+  }
+
+  newGame();
+  const whenClicked = function() {
+    //is the game active?
+    if (!isGameActive) {
+      alert("game over! press the NEW GAME button");
+      return;
+    }
+
+    // Check if square that was clicked is empty
+    if ($(this).text() !== "") {
+      //alert the user that they cant click twice on a buton
+      alert("you cant click twice, sorry!");
+      return;
+    }
+
+    if (currentPlayer === "X") {
+      $(this).text("X");
+      currentPlayer = "0";
+      idsOfCrosses.push($(this).attr("id"));
+    } else {
+      $(this).text("0");
+      currentPlayer = "X";
+      idsOfNods.push($(this).attr("id"));
+    }
+
+    // Check if there any winning combinations for 0
+    if (idsOfNods.includes("0") && idsOfNods.includes("1") && idsOfNods.includes("2")) {
+      console.log(`0 wins`);
+      isGameActive = false;
+      return `0 wins`;
+    }
+    if (idsOfNods.includes("2") && idsOfNods.includes("4") && idsOfNods.includes("6")) {
+      console.log(`0 wins`);
+      isGameActive = false;
+
+      return `0 wins`;
+    }
+    if (idsOfNods.includes("0") && idsOfNods.includes("4") && idsOfNods.includes("8")) {
+      console.log(`0 wins`);
+      isGameActive = false;
+
+      return `0 wins`;
+    }
+    if (idsOfNods.includes("3") && idsOfNods.includes("4") && idsOfNods.includes("5")) {
+      console.log(`0 wins`);
+      isGameActive = false;
+
+      return `0 wins`;
+    }
+    if (idsOfNods.includes("6") && idsOfNods.includes("7") && idsOfNods.includes("8")) {
+      console.log(`0 wins`);
+      isGameActive = false;
+
+      return `0 wins`;
+    }
+
+    // Check if there any winning combinations for X
+    if (idsOfCrosses.includes("0") && idsOfCrosses.includes("1") && idsOfCrosses.includes("2")) {
+      console.log(`X wins`);
+      isGameActive = false;
+
+      return `X wins`;
+    }
+    if (idsOfCrosses.includes("2") && idsOfCrosses.includes("4") && idsOfCrosses.includes("6")) {
+      console.log(`X wins`);
+      isGameActive = false;
+
+      return `X wins`;
+    }
+    if (idsOfCrosses.includes("0") && idsOfCrosses.includes("4") && idsOfCrosses.includes("8")) {
+      console.log(`X wins`);
+      isGameActive = false;
+
+      return `X wins`;
+    }
+    if (idsOfCrosses.includes("3") && idsOfCrosses.includes("4") && idsOfCrosses.includes("5")) {
+      console.log(`X wins`);
+      isGameActive = false;
+
+      return `X wins`;
+    }
+    if (idsOfCrosses.includes("6") && idsOfCrosses.includes("7") && idsOfCrosses.includes("8")) {
+      console.log(`X wins`);
+      isGameActive = false;
+
+      return `X wins`;
+    }
+
+    // TODO check if there are no empty squares left and alert GAME OVER
+    let numberOfEmptySquares = $(".box:empty").length;
+    console.log(`There are ${numberOfEmptySquares} empty squares`);
+
+    if (numberOfEmptySquares < 1) {
+      alert("GAME OVER - It's a draw");
+    }
+
+
+
+  }
 
 
   $(".box").on("click", whenClicked);
+  $("#newGame").on("click", newGame);
 });
-// 
-// '#' + idsOfNods.join(', #')
-// "#0, #1, #2"
-// $( '#' + idsOfNods.join(', #') )
-// jQuery.fn.init(3) [div#0.box, div#1.box, div#2.box, prevObject: jQuery.fn.init(1)]
-// $( '#' + idsOfNods.join(', #') ).addClass('greeny')
-// jQuery.fn.init(3) [div#0.box.greeny, div#1.box.greeny, div#2.box.greeny, prevObject: jQuery.fn.init(1)]
-
-
-
-//this.attr("id")[3]
